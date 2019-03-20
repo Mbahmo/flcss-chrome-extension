c9f411c42413afe6ea1ae63488e82c650433cafb
Yongki Agustinus <yong@Yongkis-MacBook-Pro.local>
2019-03-20 18:40:23 +0800

    Add loadcss function

--- content.js
+++ content.js
@@ -3,17 +3,24 @@ var tabUrl = document.location.href;
 // Only run if its findlaw site and not in admin
 if(tabUrl.indexOf("findlaw1.flsitebuilder.com") >= 0 && tabUrl.indexOf("wp-admin") <= 1 && tabUrl.indexOf("wp-content") <= 1 ){
 
-	console.log( 'FindLaw site detected' );
+	var source = document.head.querySelector("[name~=flcss_source][content]").content;
+
+	console.log( 'FindLaw site ' + siteID );
+	console.log( 'Source: ' + source );
 
 	var siteID = getSiteID( tabUrl );
-	console.log( siteID );
-	addcss(  );
 
-	var link = document.createElement("link");
-	link.href = "https://flcss.smplwp.com/site/"+ siteID +"/?l=css";
-	link.type = "text/css";
-	link.rel = "stylesheet";
-	document.getElementsByTagName("head")[0].appendChild(link);
+	/**
+	 * If source is remote, load remote css
+	 */
+	if( source == 'remote' ){
+		loadCss( 'https://flcssgit.smplwp.com/' + siteID + '/general.css' );
+		loadCss( 'https://flcssgit.smplwp.com/' + siteID + '/header.css' );
+		loadCss( 'https://flcssgit.smplwp.com/' + siteID + '/footer.css' );
+		loadCss( 'https://flcssgit.smplwp.com/' + siteID + '/home.css' );
+		loadCss( 'https://flcssgit.smplwp.com/' + siteID + '/internal.css' );
+		loadCss( 'https://flcssgit.smplwp.com/' + siteID + '/blog.css' );
+	}
 
 }
 
@@ -25,20 +32,10 @@ function getSiteID( url ){
 	return siteID;
 }
 
-/**
- * Load CSS
- */
-function addcss(css){
-	var head = document.getElementsByTagName('head')[0];
-	var s = document.createElement('style');
-	s.setAttribute('type', 'text/css');
-	s.setAttribute('href', css);
-	if (s.styleSheet) {   // IE
-	//s.styleSheet.cssText = css;
-	} else {                // the world
-	}
-	head.appendChild(s);
+function loadCss(url){
+	var link 	= document.createElement("link");
+	link.href 	= url;
+	link.type 	= "text/css";
+	link.rel 	= "stylesheet";
+	document.getElementsByTagName("head")[0].appendChild(link);
 }
-
-
-//http://flcss.smplwp.com/site/3090097/?l=css
